e271dcddeeaa1a2cb82a2af7767a55e2134ac77c
Carlos <dominguezalacid@gmail.com>
2025-03-28 09:48:01 +0100

    actualizar

--- Print-PDF/script.js
+++ Print-PDF/script.js
@@ -4,10 +4,33 @@ document.getElementById('download').addEventListener('click', () => {
     const options = {
         margin: 0,
         filename: 'CV-Carlos-Fernandez.pdf',
-        image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 2, useCORS: true },
+        image: { type: 'jpeg', quality: 1 },
+        html2canvas: { 
+            scale: 3, 
+            useCORS: true,
+            scrollY: 0 
+        },
         jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
     };
     
     html2pdf().from(cvElement).set(options).save();
 });
+
+
+// document.getElementById('download').addEventListener('click', () => {
+//     const cvElement = document.getElementById('page1');
+
+//     const options = {
+//         margin: 5, // Márgenes más pequeños para aprovechar el espacio
+//         filename: 'CV-Carlos-Fernandez.pdf',
+//         image: { type: 'jpeg', quality: 1 }, // Aumenta la calidad de imagen
+//         html2canvas: { 
+//             scale: 3, // Mejora la resolución del renderizado
+//             useCORS: true,
+//             scrollY: 0
+//         },
+//         jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+//     };
+
+//     html2pdf().from(cvElement).set(options).save();
+// });
